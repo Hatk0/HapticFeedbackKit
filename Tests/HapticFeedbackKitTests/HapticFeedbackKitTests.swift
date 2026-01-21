3b6f2ea8928a0dc41d17d6703ca6309470f41d27
Dmitry Yastrebov <stewielime2018@gmail.com>
2026-01-21 18:51:07 +0500

    test: add engine delegation test

--- Tests/HapticFeedbackKitTests/HapticFeedbackKitTests.swift
+++ Tests/HapticFeedbackKitTests/HapticFeedbackKitTests.swift
@@ -1,12 +1,31 @@
 import XCTest
 @testable import HapticFeedbackKit
 
+@MainActor
 final class HapticFeedbackKitTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    func testEngineDelegatesToProvider() {
+        let provider = TestProvider()
+        let engine = HapticEngine(provider: provider)
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        engine.prepare(.selection)
+        engine.play(.selection)
+
+        XCTAssertEqual(provider.prepareCount, 1)
+        XCTAssertEqual(provider.playCount, 1)
+        XCTAssertTrue(engine.supportsHaptics)
+    }
+}
+
+private final class TestProvider: HapticProvider {
+    var supportsHaptics: Bool { true }
+    var prepareCount = 0
+    var playCount = 0
+
+    func prepare(_ pattern: HapticPattern) {
+        prepareCount += 1
+    }
+
+    func play(_ pattern: HapticPattern) {
+        playCount += 1
     }
 }
